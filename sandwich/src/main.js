5aa39ba1f3b014d411ac0f8d9d3b9cf903b762d3
No1 <ayoakintilo@gmail.com>
2024-09-06 06:23:50 +0000

    renamed:    sandwich/main.js -> sandwich/src/main.js

--- sandwich/src/main.js
+++ sandwich/src/main.js
@@ -1,7 +1,7 @@
 // public/main.js
 console.log('main.js loaded');
 
-import './src/photoUpload.js';
+import './photoUpload.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Application initialized');
